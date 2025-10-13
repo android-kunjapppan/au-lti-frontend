60b4398945b1b47425ec03d6c17d69898a7a1383
Yogi Gabani <ygabani@asu.edu>
2025-10-13 20:13:52 +0400

    chore: removed unwanted changes which pushed by mistake previously

--- plugins/useTweakpane.client.ts
+++ plugins/useTweakpane.client.ts
@@ -4,9 +4,9 @@ const pane = new Pane();
 /** @description Provides a sharable instance of a tweakpane pane/ control panel. */
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig();
-  // if (runtimeConfig.public.stage !== "dev") {
-  pane.hidden = true;
-  // }
+  if (runtimeConfig.public.stage !== "dev") {
+    pane.hidden = true;
+  }
   return {
     provide: {
       pane: pane,
